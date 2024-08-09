fd5c0ee487d9aed3ac55663c9aad4e89c9f09ebf
Hamsa <hamsamuse.hm@gmail.com>
2024-08-09 12:16:39 +0100

    give it a go and pray it works

--- encrypt.js
+++ encrypt.js
@@ -2,7 +2,6 @@ import crypto from "crypto";
 
 // type in your answer in the update function
 
-let encrypted_answer= crypto.createHash('sha256').update('Sint Maarten').digest('hex');
+let encrypted_answer= crypto.createHash('sha256').update('').digest('hex');
 //                                                       ^
 
-console.log(encrypted_answer)
