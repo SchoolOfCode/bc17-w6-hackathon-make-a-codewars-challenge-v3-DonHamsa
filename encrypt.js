baf5132d3a0b10ed15dfbc9e390fbd8b8f079824
Hamsa <hamsamuse.hm@gmail.com>
2024-08-09 13:09:28 +0100

    finished

--- encrypt.js
+++ encrypt.js
@@ -2,6 +2,7 @@ import crypto from "crypto";
 
 // type in your answer in the update function
 
-let encrypted_answer= crypto.createHash('sha256').update('').digest('hex');
+let encrypted_answer= crypto.createHash('sha256').update('Sint Maarten').digest('hex');
 //                                                       ^
 
+console.log(encrypted_answer)
